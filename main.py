be97cf796b0c470caa0573f950b07802ecd77978
Apostolos Tselios <apostolis.tselios@gmail.com>
2019-07-27 01:47:20 +0300

    Add functionality to the Register button in the Welcome page.
    Rename login_page.ui to login_reg_page.ui.

--- main.py
+++ main.py
@@ -10,30 +10,51 @@ class WelcomePage(QWidget):
         loadUi('./ui/welcome_page.ui', self)
 
         self.login_button.clicked.connect(self.login_clicked)
+        self.register_button.clicked.connect(self.register_clicked)
 
     def login_clicked(self):
-        user = 'Apostolis'
-        password = 'Me'
-        self.login_page = LoginPage(user, password)
-        self.close()
+        print(self.sender())
+        self.login_page = LoginRegisterPage(self.sender())
         self.login_page.show()
 
+    def register_clicked(self):
+        print(self.sender())
+        self.register_page = LoginRegisterPage(self.sender())
+        self.register_page.show()
 
-class LoginPage(QWidget):
 
-    def __init__(self, user=None, password=None):
+class LoginRegisterPage(QWidget):
+
+    def __init__(self, sender):
         super().__init__()
-        loadUi('./ui/login_page.ui', self)
-        self.user = user
-        self.password = password
-        print(self.user, self.password)
+        loadUi('./ui/login_reg_page.ui', self)
 
-        self.cancel_button.clicked.connect(self.cancel_clicked)
+        if sender.text() == 'Register':
+            self.header_label.setText('Register')
+            self.login_reg_button.setText('Register')
+            self.login_reg_button.clicked.connect(self.register_clicked)
+        else:
+            self.login_reg_button.clicked.connect(self.login_clicked)
 
-    def cancel_clicked(self):
-        self.welcome_page = WelcomePage()
-        self.close()
-        self.welcome_page.show()
+        self.cancel_button.clicked.connect(self.close)
+
+    def login_clicked(self):
+        self.username = self.username_txt.text()
+        self.password = self.password_txt.text()
+
+        print('Username: %s Password: %s' % (self.username, self.password))
+
+    def register_clicked(self):
+        self.username = self.username_txt.text()
+        self.password = self.password_txt.text()
+
+        print('Username: %s Password: %s' % (self.username, self.password))
+
+
+class RegisterPage(QWidget):
+
+    def __init__(self):
+        super().__init__()
 
 
 class Page(QWidget):
